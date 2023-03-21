9489765700e698bb109bbb855c549942c518f864
Kamalesh-Seervi <kkamalesh117@gmail.com>
2023-03-21 23:45:25 +0530

    volume part1

--- data/main.sh
+++ data/main.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-git clone https://git.selfmade.ninja/KamaleshSeervi/photogram2.git /var/www/html
+# git clone https://git.selfmade.ninja/KamaleshSeervi/photogram2.git /var/www/html
 mv /var/photogram/photogramconfig.json /var/www
 sed -i "s/short_open_tag = .*/short_open_tag = On/" /etc/php/8.1/apache2/php.ini
 
